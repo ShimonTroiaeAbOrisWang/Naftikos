0ec8be7d8781eb347bcf0bae918433d404c926d3
xiao9905 <xiao190525@gmail.com>
2019-08-29 11:42:50 +0800

    create class SQLite

---
+++ app/src/main/java/com/example/prototype/SQLiteDao.java
@@ -0,0 +1,99 @@
+package com.example.prototype;
+
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class SQLiteDao {
+    private String name;
+    private SQLiteDatabase db;
+
+    public SQLiteDao() {
+        name = MainActivity.getContext().getExternalFilesDir("") + "/news.db";
+        db = SQLiteDatabase.openOrCreateDatabase(name, null);
+        db.execSQL("create table if not exists news (newsId varchar(20), category varchar(20), collection varchar(5), jsonData varchar(2000))");
+        db.close();
+    }
+
+    public void add(String newsId, String category, String collection, String jsonData) {
+        if (!find(newsId)) {
+            db = SQLiteDatabase.openOrCreateDatabase(name, null);
+            db.execSQL("insert into news (newsId,category,collection,jsonData) values (?,?,?,?)", new String[]{newsId, category, collection, jsonData});
+            db.close();
+        }
+    }
+
+    public boolean find(String newsId) {
+        db = SQLiteDatabase.openOrCreateDatabase(name, null);
+        Cursor cursor = db.rawQuery("select * from news where newsId=?", new String[]{newsId});
+        boolean b = cursor.moveToNext();
+        cursor.close();
+        db.close();
+        return b;
+    }
+
+    public void update(String newsId, String collection){
+        db = SQLiteDatabase.openOrCreateDatabase(name, null);
+        db.execSQL("update news set collection=? where newsId=?", new String[]{collection, newsId});
+        db.close();
+    }
+
+    public void delete(String newsId){
+        db = SQLiteDatabase.openOrCreateDatabase(name, null);
+        db.execSQL("delete from news where newsId=?", new String[]{newsId});
+        db.close();
+    }
+
+    public List<News> findAllInCollection() {
+        db = SQLiteDatabase.openOrCreateDatabase(name, null);
+        List<News> newsList = new ArrayList<>();
+        Cursor cursor = db.rawQuery("select * from news where collection=?", new String[]{"1"});
+        while(cursor.moveToNext()){
+            String newsId = cursor.getString(cursor.getColumnIndex("newsId"));
+            String category = cursor.getString(cursor.getColumnIndex("category"));
+            String collection = cursor.getString(cursor.getColumnIndex("collection"));
+            String jsonData = cursor.getString(cursor.getColumnIndex("jsonData"));
+            News news = new News(newsId, category, collection, jsonData);
+            newsList.add(news);
+        }
+        cursor.close();
+        db.close();
+        return newsList;
+    }
+
+    public List<News> findAllInCategory(String category){
+        db = SQLiteDatabase.openOrCreateDatabase(name, null);
+        List<News> newsList = new ArrayList<>();
+        Cursor cursor = db.rawQuery("select * from news where category=?", new String[]{category});
+        while(cursor.moveToNext()){
+            String newsId = cursor.getString(cursor.getColumnIndex("newsId"));
+            String collection = cursor.getString(cursor.getColumnIndex("collection"));
+            String jsonData = cursor.getString(cursor.getColumnIndex("jsonData"));
+            News news = new News(newsId, category, collection, jsonData);
+            newsList.add(news);
+        }
+        cursor.close();
+        db.close();
+        return newsList;
+    }
+
+    public class News {
+        public String newsId;
+        public String category;
+        public String collection;
+        public String jsonData;
+
+        public News() {
+        }
+
+        public News(String newsId, String category, String collection, String jsonData) {
+            this.newsId = newsId;
+            this.category = category;
+            this.collection = collection;
+            this.jsonData = jsonData;
+        }
+    }
+
+}
